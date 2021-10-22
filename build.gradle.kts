5cfdb0a7eeec7716b38ea4a66faab0ea407a7dea
Tim Mattison <tim@mattison.org>
2021-10-22 11:31:54 -0400

    Updated to Java 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ extensions.findByName("buildScan")?.withGroovyBuilder {
 idea.module.isDownloadSources = true
 idea.module.isDownloadJavadoc = true
 
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(8))
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(11))
 
 val gradleDependencyVersion = "7.2"
 
